8067f17a8981e440fb564e03883d27b060636850
Goodwil Gwala <41477015+GoodwillGwala@users.noreply.github.com>
2022-06-21 16:23:33 +0200

    Add files via upload

---
+++ State Machine/StateIdentifiers.h
@@ -0,0 +1,24 @@
+/**
+*@namespace <States>
+*@brief This namespace encapsulates state indentities
+*/
+#ifndef STATEIDENTIFIERS_H
+#define STATEIDENTIFIERS_H
+
+namespace States
+{
+	/**
+    *@enum <ID>
+    *@brief enums indentify different states
+    */
+	enum ID
+	{
+		None,
+		Splash,
+		Game,
+		Pause,
+		GameOver
+	};
+}
+
+#endif
